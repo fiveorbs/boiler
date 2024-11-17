06151d3d6c9b657974d272130ea1e002d8d7ac7b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-17 14:17:10 +0100

    Fix .php-cs-fixer.dist.php imports

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Finder;
-use VacantPlanet\Development\PhpCsFixer\Config;
+use FiveOrbs\Development\PhpCsFixer\Config;
 
 $finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests']);
 $config = new Config();
